28aa6b37675b7acbb36446024fbf2df3e9089579
omar-ea <126994619+WalidOEa@users.noreply.github.com>
2024-12-15 16:11:58 +0000

    Added dependencies

---
+++ home-server-ws/src/websocket/WebSocketServer.java
@@ -0,0 +1,14 @@
+package websocket;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import jakarta.websocket.*;
+import jakarta.websocket.server.ServerEndpoint;
+
+public class WebSocketServer {
+
+    private static final Logger logger = LogManager.getLogger(WebSocketServer.class);
+
+
+}
